ed88c34c053d56e347a64a22ff6474083590d76d
fulldump <gerardooscarjt@gmail.com>
2019-10-31 01:15:56 +0100

    feat: add configuration

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module blogotech
 
 go 1.12
 
-require github.com/fulldump/box v0.0.2
+require (
+	github.com/fulldump/box v0.0.2
+	github.com/fulldump/goconfig v1.5.0
+)
--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/fulldump/box"
+	"github.com/fulldump/goconfig"
 )
 
 func main() {
+
+	c := struct {
+		HttpAddr string
+	}{
+		HttpAddr: ":8000", // Default http address
+	}
+
+	goconfig.Read(&c)
 
 	b := box.NewBox()
 
@@ -15,5 +27,12 @@ func main() {
 			}),
 		)
 
-	b.Serve()
+	s := &http.Server{
+		Addr:    c.HttpAddr,
+		Handler: box.Box2Http(b),
+	}
+
+	fmt.Println("Listening to ", s.Addr)
+
+	s.ListenAndServe()
 }
